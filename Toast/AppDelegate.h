3388e1ed84daccdf3af555e56f09d2abad5d862e
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-05-20 15:23:54 -0300

    Initial Commit

---
+++ Toast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Toast
+//
+//  Created by Marcelo Sampaio on 5/20/17.
+//  Copyright © 2017 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Toast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Toast
+//
+//  Created by Marcelo Sampaio on 5/20/17.
+//  Copyright © 2017 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
